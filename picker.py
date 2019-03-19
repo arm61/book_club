9328887e9134256aef5348c98fbaaaa5cbcd106e
arm61 <a.r.mccluskey@bath.ac.uk>
2019-03-19 07:22:34 +0000

    update

--- picker.py
+++ picker.py
@@ -4,7 +4,7 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-hat = ['sam', 'gary', 'tibbsy', 'neil', 'stu', 'drew']
+hat = ['sam', 'gary', 'tibbsy', 'drew', 'iain']
 hat2 = ['gary', 'tibbsy', 'iain']
 
 choose = np.random.randint(len(hat), size = 1000000)
@@ -20,6 +20,6 @@ if hat[a] in hat2:
 choose2 = np.random.randint(len(hat2), size = 1000000)
 x2, y2 = np.histogram(choose2, bins=len(hat2))
 a2 = np.argmax(x2)
-print('The film is to be choosen by:')
+#print('The film is to be choosen by:')
 time.sleep(1)
-print(hat2[a2])
+#print(hat2[a2])
